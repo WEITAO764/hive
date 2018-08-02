704b9f005ff4eb2e14e584d92c001669ef54440a
WEITAO764 <wtao@deakin.edu.au>
2018-08-03 04:59:40 +1000

    Add backend HTML content

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 var express = require("express"),
     app = express();
-
 var port = process.env.PORT || 8080;
 
+/* Default Route. Static middleware */
 app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public/backend'));
 
-app.get("/sayHello", function (request, response) {
-  var user_name = request.query.user_name;
-  response.end("Hello " + user_name + "!");
+/* Call Login Page
+*  Note: The default backend should check session status, if fail (unlogin), then should go to here.
+* */
+app.get("/login", function (request, response) {
+    response.sendFile(__dirname + '/public/backend/login.html');
 });
 
+app.get("/register", function (request, response) {
+    response.sendFile(__dirname + '/public/backend/register.html');
+});
+
+/* Application Start */
 app.listen(port);
 console.log("Listening on port ", port);
-
 require("cf-deployment-tracker-client").track();
