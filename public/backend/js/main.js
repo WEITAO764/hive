ada353df2c360f5046f888c8fa2be0920c27300d
ikhowaja <41312677+ikhowaja@users.noreply.github.com>
2018-09-28 13:24:05 +1000

    Delete main.js

--- public/backend/js/main.js
+++
@@ -1,9 +0,0 @@
-function registerType(){
-    var val=document.getElementById("registertype").value;
-    if(val==1){
-    document.getElementById("clinicblock").style.display = "block";
-    }
-    else if(val==0){
-        document.getElementById("clinicblock").style.display = "none";
-    }
-}
