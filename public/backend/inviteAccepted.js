715388cb6689affcd7643778e0087afd2cb9e941
ikhowaja <41312677+ikhowaja@users.noreply.github.com>
2018-09-28 11:59:34 +1000

    Add files via upload

--- public/backend/inviteAccepted.js
+++ public/backend/inviteAccepted.js
@@ -1,13 +1,14 @@
 function conversationStarted(conversation) {
-    //Write code to handle the conversation here
-  }
-  
-  function onInviteAccepted(conversation) {
-    conversation.localMedia.attach('#local');
-  
-    conversation.on('participantConnected', function(participant) {
-      participant.media.attach('#remote');
-  
-      conversationStarted(conversation);
-    });
-  }
+  // Here is where you add your own custom functionality.
+  console.log('The conversation has started.');
+}
+
+function onInviteAccepted(conversation) {
+  conversation.localMedia.attach('#local');
+
+  conversation.on('participantConnected', function(participant) {
+    participant.media.attach('#remote');
+
+    conversationStarted(conversation);
+  });
+}
